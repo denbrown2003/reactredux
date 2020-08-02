988689f0417b0bbfa0d9bed5ec1a8775a1b3d7dc
artem <denbrown2003@gmail.com>
2020-08-02 12:37:17 +0300

    refactor auth component

--- src/components/forms/Auth.js
+++ src/components/forms/Auth.js
@@ -1,29 +1,36 @@
 import React from 'react'
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button, Row, Col } from 'react-bootstrap'
 
 class Auth extends React.Component{
 
     render(){
         return(
-        <Form>
-            <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
+            <Row>
+                <Col xs={6} md={4}>
+                
+                    <Form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
 
-            <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
 
-            <Button variant="warning">Click</Button>
-        </Form>
+                        <Button variant="warning">Click</Button>
+                    </Form>
+
+                </Col>
+            </Row>
+        
         )
     }
 }
